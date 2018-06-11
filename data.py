4070091096498b9a2ad09c2534d37eaaefe4f94b
AhmadAzadzadeh <a.azadzadeh.aa14@gmail.com>
2018-06-12 00:43:37 +0430

    Initial Commit And add Pages

---
+++ data.py
@@ -0,0 +1,32 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Body Of Article One',
+            'author': 'Ali Ahmadi',
+            'create_date': '06-11-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Body Of Article Two',
+            'author': 'Mohammad Mohammadi',
+            'create_date': '06-11-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Body Of Article Three',
+            'author': 'Sajjad Sajjadi',
+            'create_date': '06-11-2018'
+        },
+        {
+            'id': 4,
+            'title': 'Article Four',
+            'body': 'Body Of Article Four',
+            'author': 'Vahid Vahidi',
+            'create_date': '06-11-2018'
+        }
+    ]
+    return articles
